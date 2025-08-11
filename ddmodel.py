bda1a67ffebe6aa6304fe99f1bf4f07318331b29
lgmaciel <132719840+lgmaciel@users.noreply.github.com>
2025-08-11 11:44:54 -0300

    + Cena e mapa inicial

--- ddmodel.py
+++ ddmodel.py
@@ -87,6 +87,63 @@ class Jogador():
         self.mana = 0
         self.stamina = 0  
         self.inventario = dict()
-        self.item_equipado:Item = Item()
+        self.item_equipado = Item()
+
+
+class Cena():
+    def __init__(self, nome="Indefinida"):
+        # identificação
+        self.nome = nome
+
+        # propriedades
+        self.itens = dict()
+
+        # Para o mapa: Cenas conectadas a esta cena
+        self.norte = None
+        self.sul = None
+        self.leste = None
+        self.oeste = None
+
+
+# MAPA DO JOGO
+hall = Cena("Hall")
+sala = Cena("Sala")
+calabouco1 = Cena("Calabouço 1")
+
+item1 = Item()
+calabouco1.itens[item1.get_nome()] = item1
+
+item2 = Item()
+calabouco1.itens[item2.get_nome()] = item2
+
+cela1 = Cena("Cela 1")
+calabouco2 = Cena("Calabouço 2")
+cela2 = Cena("Cela 2")
+patio = Cena("Patio")
+
+# Conectamos as CENAS
+
+hall.leste = sala
+
+sala.sul = calabouco1
+sala.oeste = hall
+
+calabouco1.leste = cela1
+calabouco1.norte = sala
+calabouco1.sul = calabouco2
+
+cela1.oeste = calabouco1
+cela1.sul = cela2
+
+calabouco2.norte = calabouco1
+calabouco2.leste = cela2
+
+cela2.norte = cela1
+cela2.oeste = calabouco2
+cela2.leste = patio
+
+patio.oeste = cela2
+
+
 
 JOGADOR = Jogador()
